965b5b95f9d3c959fc7f119f2e8e8466d5175db8
Santiago <snbedoyao@gmail.com>
2020-12-17 19:04:30 -0500

    Asignaci

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
 	<b-nav tabs>
-		<b-nav-item active>
+		<b-nav-item>
 		<router-link to="/home">Home</router-link></b-nav-item>
 		<b-nav-item>
 			<router-link to="/proyectos">Proyectos</router-link>
-		</b-nav-item>
+		</b-nav-item> 
 		<b-nav-item>
 			<router-link to="/asignacion">Asignación</router-link>
 		</b-nav-item>
--- src/components/Asignacion.vue
+++ src/components/Asignacion.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <h2>Asignación</h2>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group id="input-group-1" label="Nombre del usuario:" label-for="input-1">
+    <h1>Asignación</h1>
+    <b-form @submit="onSubmit" @reset="onReset">
+      <b-form-group
+        id="input-group-1"
+        label="Nombre del usuario:"
+        label-for="input-1"
+      >
         <b-form-input
           id="input-1"
           v-model="form.name"
@@ -11,7 +15,11 @@
         ></b-form-input>
       </b-form-group>
 
-	  <b-form-group id="input-group-2" label="Proyecto a asignar" label-for="input-2">
+      <b-form-group
+        id="input-group-2"
+        label="Proyecto a asignar"
+        label-for="input-2"
+      >
         <b-form-input
           id="input-2"
           v-model="form.proyect"
@@ -20,41 +28,38 @@
         ></b-form-input>
       </b-form-group>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
+      <b-button type="submit" variant="primary">Asignar</b-button>
+      <b-button type="reset" variant="danger">Reiniciar campos</b-button>
     </b-form>
-    <b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ form }}</pre>
-    </b-card>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       form: {
-		name: "",
-		proyect: "",
+        name: "",
+        proyect: "",
       },
       show: true,
     };
   },
   methods: {
     onSubmit(event) {
-      event.preventDefault();
-      alert(JSON.stringify(this.form));
+	  event.preventDefault();
+	  
+      axios.post('https://epyme.herokuapp.com/proyect/addUser/', {
+        	nombre: this.form.name,
+        	nombreP: this.form.proyect,
+        }).then((response) => console.log(response.data), this.onReset)
+        .catch((e) => console.log("Error!: " + e));       
     },
     onReset(event) {
       event.preventDefault();
-      // Reset our form values
       this.form.name = "";
       this.form.proyect = "";
-      // Trick to reset/clear native browser form validation state
-      this.show = false;
-      this.$nextTick(() => {
-        this.show = true;
-      });
     },
   },
 };
